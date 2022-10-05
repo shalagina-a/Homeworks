82d61110f55b90eab986612403328b707ac6422b
Anna.Shalagina@urfu.me <Anna.Shalagina@urfu.me>
2022-10-05 19:51:21 +0500

    Дз1

---
+++ Стихотворение/Стихотворение/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Андрей Белый");
+
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Один");
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Окна запотели.");
+        Console.WriteLine("На дворе луна.");
+        Console.WriteLine("И стоишь без цели");
+        Console.WriteLine("У окна.");
+
+
+        Console.WriteLine("Ветер.Никнет, споря,");
+        Console.WriteLine("Ряд седых берез.");
+        Console.WriteLine("Много было горя...");
+        Console.WriteLine("Много слез...");
+
+        Console.WriteLine("И встает невольно");
+        Console.WriteLine("Скучный ряд годин.");
+        Console.WriteLine("Сердцу больно, больно...");
+        Console.WriteLine("Я один.");
+
+
+        Console.ResetColor();
+        Console.ReadKey();
+    }
+}
