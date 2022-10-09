21010217d9a2f245c9acf857cf6517615fbc20ae
Anna.Shalagina@urfu.me <Anna.Shalagina@urfu.me>
2022-10-09 19:12:35 +0500

    ДЗ-03

---
+++ Время/Время/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество секунд:");
+            int n = int.Parse(Console.ReadLine());
+            int h = n/3600;
+            int m = (n - h * 3600) / 60;
+            int s = n - h * 3600 - m * 60;
+
+
+            Console.WriteLine(h + " часов");
+            Console.WriteLine(m + " минут");
+            Console.WriteLine(s + " секунд");
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
