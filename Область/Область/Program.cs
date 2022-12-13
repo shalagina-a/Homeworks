a5f8ae1b77d91ccfea3ff00e1ff1a3e126613f8e
Anna.Shalagina@urfu.me <Anna.Shalagina@urfu.me>
2022-12-13 12:31:06 +0500

    ДЗ-7.2.

---
+++ Область/Область/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Area
+{
+    class Program
+    {
+        static bool Area(double x, double y)
+        {
+            return (-1 <= x && x <= 1.5 && -0.5 <= y && y <= 2);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты x и y");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            bool result = Area(x, y);
+            Console.WriteLine("Точка с координатами x и y принадлежит к данной области - " + result);
+            Console.ReadKey();
+        }
+    }
+}
