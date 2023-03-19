e58ac5f2273a513397d4242adf27db37c66bcdd1
Anna.Shalagina@urfu.me <Anna.Shalagina@urfu.me>
2023-03-19 13:09:59 +0500

    ДЗ-12

---
+++ HRLibrary/HRLibrary/Subscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Subscriber
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public readonly Contract Number ;
+
+        public string GetInfo() 
+        { 
+            
+
+            return $"{Name} {Surname}. Телефонный номер: {Birthday:d}. " + $"Пол: {gender}. Возраст: {Age}."; 
+        }
+    }
+}
