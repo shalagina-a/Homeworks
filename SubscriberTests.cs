a9efcc28921b0c7baaad4f3254711bb63e15a1f9
shalagina-a <114292533+shalagina-a@users.noreply.github.com>
2023-05-14 18:34:29 +0500

    Add files via upload

---
+++ SubscriberTests.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using System.Reflection;
+
+namespace HRLibrary.UnitTests_
+{
+    [TestFixture]
+    public class Tests
+    {
+        static Subscriber sub;
+
+        [SetUp]
+        public void Setup()
+        {
+            sub = new Subscriber("Jonh", "Smith", 8119480348, 54542);
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.That(sub.Name, Is.EqualTo("John"));
+            Assert.That(sub.Surname, Is.EqualTo("Smith"));
+            Assert.That(sub.PhoneNumber, Is.EqualTo(8119480348));
+            Assert.That(sub.ContractNumber, Is.EqualTo(54542));
+        }
+
+        [Test]
+        public void TariffNameGetter_Subscriber()
+        {
+            Assert.That(sub.TariffName, Is.EqualTo("Smart"));
+        }
+
+        [Test]
+        public void PaymentTypeGetter_Subscriber()
+        {
+            Assert.That(sub.PaymentType, Is.EqualTo(PaymentType.Credit));
+        }
+
+        public void SumGetter_Subscriber()
+        {
+            Assert.That(sub.Sum, Is.EqualTo(1223));
+        }
+
+        [Test]
+        public void GetInfo_Subscriber_ValuesString()
+        {
+            string expectedInfo = "John Smith. ";
+            expectedInfo += "Телефонный номер: 8119480348. ";
+            expectedInfo += "Номер договора: 54542. ";
+            expectedInfo += $"Название тарифа: Smart. ";
+            expectedInfo += $"Тип оплаты: {PaymentType.Credit}. ";
+            expectedInfo += $"Сумма на личном счете: 1223.";
+            Assert.That(sub.GetInfo(), Is.EqualTo(expectedInfo));
+        }
+
+        [Test]
+        public void CompareToTest()
+        {
+            var alexander = new Subscriber("Александр", "Богданов", 742701079, 6645);
+            var mihail = new Subscriber("Михаил", "Некрасов", 744036776, 633);
+            var alisa = new Subscriber("Алиса", "Князева", 789997127, 902);
+            var jaroslav = new Subscriber("Ярослав", " Поляков", 792859559429, 6);
+            var maria = new Subscriber("Мария", "Афанасьева", 792546915726, 5);
+
+
+            Assert.That(alisa.CompareTo(alexander), Is.LessThan(0));
+            Assert.That(alexander.CompareTo(mihail), Is.GreaterThan(0));
+            Assert.That(jaroslav.CompareTo(maria), Is.LessThan(0));
+            Assert.That(maria.CompareTo(maria), Is.EqualTo(0));
+        }
+    }
+}
